2947fb02323c67338294ffa5d0c69c3becbb95f2
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-09 19:00:07 +0530

    css added

--- Game_Journey/src/components/header.jsx
+++ Game_Journey/src/components/header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return(
         <header>
-            <img src="../public/assets/logo.png" alt="logo" width="40px"/>
+            <img src="../public/assets/logo.png" alt="logo"width="10px" />
             <h1>Game-Journey</h1>
         </header>
     )
