9a92ed8e7c5e1e4d11af5de8de74f02b0c68be77
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-09 18:58:55 +0530

    CSS added

--- Game_Journey/src/App.jsx
+++ Game_Journey/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/header";
+import Entry from "./components/entry";
 
 export default function App() {
   return(
     <div id="content">
       <Header/>
+      <Entry />
     </div>
     
   )
