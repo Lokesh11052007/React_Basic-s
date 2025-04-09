995d019b02b928e78d0745616e0dce7f0836433e
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-09 16:58:59 +0530

    Header added

---
+++ Game_Journey/src/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return(
+        <header>
+            <img src="../public/assets/logo.png" alt="logo" width="40px"/>
+            <h1>Game-Journey</h1>
+        </header>
+    )
+}
