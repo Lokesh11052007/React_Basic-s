04aa7f0ee21faab63a2bacc62f8ea2878f451d68
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-09 19:30:22 +0530

    small tgs added

--- Game_Journey/src/components/entry.jsx
+++ Game_Journey/src/components/entry.jsx
@@ -2,15 +2,24 @@ export default function entry() {
     return (
         <article className="journal-entry">
             <div className= "main-image-container">
-                <img className="main-image" src="../public/assets/pubg.jpg" alt="pubg" width="100px"/>
+                <img className="main-image" 
+                src="../public/assets/pubg.jpg"
+                alt="pubg"
+                />
             </div>
             <div className="entry-container">
-                <img src="../public/assets/andriod.png" alt="android" width="20px"/>
-                <span>android</span>
-                <a href="https://pubg.com/en/main"></a>
+                <div>
+                    <img src="../public/assets/andriod.png"
+                        alt="android" 
+                        width="20px"
+                        />
+                    <span>android</span>
+                </div>
+                <a href="https://pubg.com/en/main">Download Now</a>
                 <h2 className="entry-title">PUBG</h2>
-                <p>24 Sep 2018</p>
-                <p>BATTLEGROUNDS is a battle royale that pits 100 players against each other. Outplay your opponents to become the lone survivor. Play free now!</p>
+                <h4>24 Sep 2018 - Current</h4>
+                <p>BATTLEGROUNDS is a battle royale that pits 100 players against each other.
+                Outplay your opponents to become the lone survivor. Play free now!</p>
             </div>
         </article>
     )
