2e846e3a92bf6cb13e32704b03f73fed317241ee
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-10 22:47:19 +0530

    created header

---
+++ Chef_Claude/src/App.jsx
@@ -0,0 +1,10 @@
+import chef from "./assets/chef.png"
+
+export default function App() {
+  return (
+    <header>
+      <img src={chef} alt="chef logo" />
+      <h1>Chef Claude</h1>
+    </header>
+  )
+}
