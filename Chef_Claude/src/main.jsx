bd5b2486d978a1d40f8c9f9be20085a54ca78cad
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-11 14:46:01 +0530

    useState to add Ingredient

--- Chef_Claude/src/main.jsx
+++ Chef_Claude/src/main.jsx
@@ -1,10 +1,32 @@
+import React from "react";
+
 export default function Main() {
+    const [ingredient, setIngredient] = React.useState([]);
+    const mapping = ingredient.map(item => (
+        <li key={item}> {item} </li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("Ingredient")
+        
+        setIngredient(prevIngredient => [...prevIngredient, newIngredient])
+    }
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input type="text" placeholder="e.g: oregano"/>
+            <form onSubmit={handleSubmit}  className="add-ingredient-form">
+                <input 
+                  type="text"
+                  placeholder="e.g: oregano"
+                  aria-label="Add Ingredient"
+                  name="Ingredient"
+                  />
                 <button>Add Ingredient</button>
             </form>
+            <ul>
+                {mapping}
+            </ul>
         </main>
     )
 }
