a74eab6c7c690d49b224a518674bb457dfd1e179
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-12 12:24:42 +0530

    Components folder

---
+++ Chef_Claude/src/components/IngredientsList.jsx
@@ -0,0 +1,18 @@
+export default function IngredientsList(props){
+    const mapping = props.ingredient.map(item => (
+        <li key={item}> {item} </li>
+    ))
+    return (
+        <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{mapping}</ul>
+                {props.ingredient.length > 5 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredient.</p>
+                    </div>
+                    <button onClick={props.toggleButton} >Get a recipe</button>
+                </div>}
+            </section>
+    )
+}
